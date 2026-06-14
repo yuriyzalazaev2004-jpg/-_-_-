2281b1d806507f906ac742963fcf88b9a77d8b9e
yuriyzalazaev2004-jpg <yuriyzalazaev2004@gmail.com>
2026-06-14 22:32:01 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ const services = [
   }
 ];
 
-const reviewImages = Array.from({ length: 9 }, (_, i) => ({
+const reviewImages = Array.from({ length: 25 }, (_, i) => ({
   src: `images/reviews/review-${i + 1}.jpg`,
   alt: `Отзыв клиента ${i + 1}`
 }));
